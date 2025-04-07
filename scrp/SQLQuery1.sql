a73d053132326f079c358bff279bff48ce77e553
HengUUU <165640532+HengUUU@users.noreply.github.com>
2025-04-07 11:39:37 +0700

    Add files via upload

--- scrp/SQLQuery1.sql
+++ scrp/SQLQuery1.sql
@@ -1,6 +1,6 @@
 /* 
 ==========
-create database and schema
+create database and schema,  and table
 ==========
 scipts pupose 
 this scrips is used to create database and schemas( brown, silver, and gold).
@@ -27,3 +27,71 @@ create schema silver;
 go
 create schema gold;
 go
+
+
+
+if object_id ('bronze.crm_cust_info', 'U') is not null
+	drop table bronze.crm_cust_info;
+create table bronze.crm_cust_info(
+	cst_id int,
+	cst_firstname varchar(20),
+	cst_marital_status char(2),
+	cst_gndr char(2),
+	cst_create_date Date
+
+);
+go
+
+
+
+if object_id ('bronze.crm_prd_info', 'U') is not null
+	drop table bronze.crm_prd_info;
+
+create table crm_prd_info(
+	prd_id int,
+	prd_key varchar(30),
+	prd_nm varchar(30),
+	prd_cost int,
+	prd_line nvarchar(2),
+	prd_start_dt Date,
+	prd_end_dt Date
+);
+go
+
+create table crm_sale_details (
+sls_ord_nm nvarchar(20),
+sls_prd_key nvarchar(20),
+sls_cust_id int,
+sls_order_dt int,
+sls_ship_dt int,
+sls_due_dt int,
+sls_sales int,
+sls_quantity int,
+sls_price float
+);
+go
+
+
+create table erp_cust_az12 (
+cid int,
+bdate Date,
+gen nvarchar(2)
+);
+go
+
+
+create table erp_loc_A101 (
+cid int,
+cntry nvarchar(20)
+
+);
+go
+
+create table erp_px_cat_g1v2 (
+id nvarchar(10),
+cat nvarchar(20),
+subcat nvarchar(20),
+maintenance nvarchar(10)
+);
+go
+
