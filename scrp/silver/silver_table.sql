d8713ed42ec958dfd73c86f580fc436f0c2042c2
HengUUU <165640532+HengUUU@users.noreply.github.com>
2025-04-11 16:53:59 +0700

    Update silver_table.sql

--- scrp/silver/silver_table.sql
+++ scrp/silver/silver_table.sql
@@ -28,7 +28,6 @@ create table silver.crm_cust_info(
 go
 
 
-
 if object_id ('silver.crm_prd_info', 'U') is not null
 	drop table silver.crm_prd_info;
 create table silver.crm_prd_info(
@@ -36,7 +35,8 @@ create table silver.crm_prd_info(
 	prd_key varchar(50),
 	prd_nm varchar(50),
 	prd_cost int,
-	prd_line nvarchar(15),
+	prd_line nvarchar(20),
+	cat_id nvarchar(20),
 	prd_start_dt Date,
 	prd_end_dt Date,
 	dwh_create_date Datetime2 default getdate()
