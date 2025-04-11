3953ae8cf3bc8b037ea3cdcd1837854b8627dae1
HengUUU <165640532+HengUUU@users.noreply.github.com>
2025-04-11 16:55:52 +0700

    Update standardize&insert_data.sql

--- scrp/silver/standardize&insert_data.sql
+++ scrp/silver/standardize&insert_data.sql
@@ -38,3 +38,35 @@ from bronze.crm_cust_info
 )t
 where row_num = 1; -- select the last update row 1 (desc order)
 go 
+
+
+
+--- insert data into silver.crm_prd_info
+
+truncate table silver.crm_prd_info;
+insert into silver.crm_prd_info(
+prd_id,
+cat_id,
+prd_key,
+prd_nm,
+prd_cost,
+prd_line,
+prd_start_dt,
+prd_end_dt
+)
+select prd_id,
+replace(substring(prd_key,1,5), '-', '_') as cat_id,
+substring(prd_key, 7, len(prd_key)) as prd_key,
+prd_nm,
+isnull(prd_cost, 0) as prd_cost,
+case 
+	when upper(trim(prd_line)) = 'M' then 'Mountian'
+	when upper(trim(prd_line)) = 'R' then 'Road'
+	when upper(trim(prd_line)) = 'S' then 'Other Sales'
+	when upper(trim(prd_line)) = 'T' then 'Touring'
+	else 'n/a'
+end prd_line,
+prd_start_dt,
+dateadd(day,-1,lead(prd_start_dt) over (partition by prd_key order by prd_start_dt) ) as prd_end_dt
+from bronze.crm_prd_info;
+go
