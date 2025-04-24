90f96f977d2f354fc7b1be27602bfeece4c35db9
HengUUU <165640532+HengUUU@users.noreply.github.com>
2025-04-24 14:56:05 +0700

    Update standardize&insert_data.sql

--- scrp/silver/standardize&insert_data.sql
+++ scrp/silver/standardize&insert_data.sql
@@ -63,3 +63,41 @@ prd_start_dt,
 dateadd(day,-1,lead(prd_start_dt) over ( partition by prd_key order by prd_start_dt)) as prd_end_dt
 from bronze.crm_prd_info;
 go
+
+
+	
+
+--- insert data into silver.crm_sale_detailts
+truncate table silver.crm_sale_details;
+insert into silver.crm_sale_details(
+sls_ord_nm, sls_prd_key, sls_cust_id, sls_order_dt, sls_ship_dt, sls_due_dt, sls_sales, sls_quantity, sls_price
+)
+select sls_ord_nm,
+sls_prd_key,
+sls_cust_id,
+case	
+	when len(sls_order_dt) < 8 then null
+	else cast(cast(sls_order_dt as varchar) as date)
+end as sls_order_dt,
+case	
+	when len(sls_ship_dt) < 8 then null
+	else cast(cast(sls_ship_dt as varchar) as date)
+end as sls_ship_dt,
+case	
+	when len(sls_due_dt) < 8 then null
+	else cast(cast(sls_due_dt as varchar) as date)
+end as sls_due_dt,
+case
+	when sls_sales is null or sls_sales < 0 or sls_sales != sls_quantity * abs(sls_price) then sls_quantity * abs(sls_price)
+	else sls_sales
+end as sls_sales,
+sls_quantity,
+case 
+	when sls_price is null or sls_price < 0 then sls_sales / nullif(sls_quantity, 0 )
+	else sls_price
+end as sls_price
+from bronze.crm_sale_details;
+go
+
+
+
